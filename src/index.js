ce0e5eecce7638f0bb5cc44907d84c93092df66f
Lily <mrs.esmirn@gmail.com>
2024-10-03 10:06:34 +0400

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsMap = {};
+  const openBrackets = new Set();
+  const sameBrackets = new Set();
+
+  for (const [open, close] of bracketsConfig) {
+    bracketsMap[close] = open;
+    openBrackets.add(open);
+    if (open === close) {
+      sameBrackets.add(open);
+    }
+  }
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (sameBrackets.has(char)) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (stack.length === 0 || stack[stack.length - 1] !== bracketsMap[char]) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
 }
